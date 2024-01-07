443d11ab64e4d2b001a5145784672e0d1f5d58ed
Tuan Anh Nguyen <tunguyen@ftoulouse.toulouse.grid5000.fr>
2024-01-07 16:01:02 +0100

    modify docker

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,32 @@
 
 ARG BASE_IMAGE
+ARG ROLE
 
 FROM $BASE_IMAGE
 
-WORKDIR /client
+WORKDIR /fl_training
 
-ADD config /client/config
-ADD client.py /client/client.py
-ADD utils.py /client/utils.py
-ADD models.py /client/models.py
-ADD requirements.txt /client/requirements.txt
+# Create a placeholder file based on ROLE
+RUN touch placeholder_file
+
+# Copy role-specific files based on ROLE
+COPY server.py /fl_training/server.py
+COPY main_server.py /fl_training/main_server.py
+
+# Check if ROLE is "client" and remove unnecessary files
+RUN if [ "$ROLE" = "client" ]; then \
+        rm -f server.py main_server.py placeholder_file; \
+    else \
+        rm -f client.py placeholder_file; \
+    fi
+
+ADD config /fl_training/config
+#ADD client.py /client/client.py
+COPY utils.py /fl_training/utils.py
+COPY models.py /fl_training/models.py
+COPY requirements.txt /fl_training/requirements.txt
 
 RUN pip3 install --upgrade pip
 RUN pip3 install --no-cache-dir -r requirements.txt
+
+ENTRYPOINT ["/bin/bash"]
--- run_docker.sh
+++ run_docker.sh
@@ -3,7 +3,7 @@
 BASE_IMAGE_PT=nvcr.io/nvidia/pytorch:23.12-py3
 BASE_IMAGE_JETSON=nvcr.io/nvidia/l4t-pytorch:r35.1.0-pth1.13-py3
 
-while getopts "b:" opt; do
+while getopts "b:r:" opt; do
   case $opt in
     b)
         case $OPTARG in 
@@ -21,6 +21,22 @@ while getopts "b:" opt; do
                 ;;
         esac
         ;;
+    r)
+        case $OPTARG in 
+            server)
+                echo "Running Flower server"
+                ROLE="server"
+                ;;
+            client)
+                echo "Running Flower client"
+                ROLE="client"
+                ;;
+            *)
+                echo "Invalid role option: $OPTARG" >&2
+                exit 1
+                ;;
+        esac
+        ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       ;;
@@ -28,7 +44,7 @@ while getopts "b:" opt; do
 done
 
 # Build the Docker image
-docker build -t flower_client:latest --build-arg BASE_IMAGE=$BASE_IMAGE .
+docker build -t flower_client:latest --build-arg BASE_IMAGE=$BASE_IMAGE --build-arg ROLE=$ROLE .
 
 # Check if the build was successful
 if [ $? -eq 0 ]; then
